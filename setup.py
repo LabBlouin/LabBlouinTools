0416413b1177ffef2d233f66d13779b18060898c
Alex Safatli <safatli@cs.dal.ca>
2014-10-17 09:41:04 -0300

    Setup for distutils.

---
+++ setup.py
@@ -0,0 +1,17 @@
+''' An installation script for these tools. '''
+
+# Date:   Oct 17 2014
+# Author: Alex Safatli
+# E-mail: safatli@cs.dal.ca
+
+from setuptools import setup
+
+VERSION = '0.1'
+DESCRIP = 'Bioinformatic python utilities modules, and libraries for use with Lab Blouin applications.'
+URL     = 'http://www.github.com/AlexSafatli/LabBlouinTools'
+AUTHOR  = 'Alex Safatli'
+EMAIL   = 'safatli@cs.dal.ca'
+DEPNDS  = ['pymol']
+LINKS   = []
+
+setup(name='labblouintools',version=VERSION,description=DESCRIP,url=URL,author=AUTHOR,author_email=EMAIL,license='MIT',packages=['abermusa'],install_requires=DEPNDS,dependency_links=LINKS,zip_safe=False)
